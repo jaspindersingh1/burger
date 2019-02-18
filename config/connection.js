c07f5959c3ae6e5d551fbf4177443e91019bd026
jaspindersingh1 <jaspinders31@gmail.com>
2019-02-18 18:31:37 -0500

    updated project and added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Please ask for Password",
-  database: "burgers_db",
-  
-});
+var connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}  
+else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Please ask for password.",
+    database: "burgers_db",
+  })
+};
 
 // Make the connection
 connection.connect(function(err) {
